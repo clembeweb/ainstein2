7c7b976cf1bc657c085e2c4ad18b5b21a5ec9912
Clemente B <clembeweb@example.com>
2025-06-13 19:09:01 +0200

    fix: restore public/index.php and map /dashboard route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::domain(config('tenancy.central_domains')[0])->group(function () {
 */
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', DashboardController::class)->name('dashboard');
+    Route::get('/dashboard', DashboardController::class)->name('dashboard'); // nuova riga
 });
